0053971a47288d949b7060d27a910da954d18e46
chriszf <chriszf@gmail.com>
2013-10-16 16:36:43 -0700

    Adding a binary

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='fume',
+        version='0.1',
+        description="Fume: a smoke test scripting language",
+        author="Christian Fernandez",
+        author_email="c@hackbrightacademy.com",
+        packages=["fume"],
+        scripts=['scripts/fume']
+        )
