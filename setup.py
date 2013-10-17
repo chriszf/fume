f66098352e15ea31afdb824644ae20d1a2a640a3
chriszf <chriszf@gmail.com>
2013-10-16 17:26:41 -0700

    Switching to setuptools arg

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from distutils.core import setup
+from setuptools import setup
 setup(name='fume',
-        version='0.1',
+        version='0.1.1',
         description="Fume: a smoke test scripting language",
         author="Christian Fernandez",
         author_email="c@hackbrightacademy.com",
         packages=["fume"],
         package_dir={"": "src"},
         scripts=['scripts/fume'],
-        requires=["docopt (>= 0.6.1)", "requests (>=2.0.0)"]
+        install_requires=["docopt>=0.6.1", "requests>=2.0.0"]
         )
