0f3b5c182caa232c4791b29068f4755366346090
chriszf <chriszf@gmail.com>
2013-10-16 17:17:09 -0700

    UGH setuptools

--- setup.py
+++ setup.py
@@ -5,5 +5,7 @@ setup(name='fume',
         author="Christian Fernandez",
         author_email="c@hackbrightacademy.com",
         packages=["fume"],
-        scripts=['scripts/fume']
+        package_dir={"": "src"},
+        scripts=['scripts/fume'],
+        requires=["docopt (>= 0.6.1)", "requests (>=2.0.0)"]
         )
